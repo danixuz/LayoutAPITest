23e98868a931d147892423c95ae02ff01b9d8cd8
danixuz <danixuz@icloud.com>
2022-07-07 00:31:51 +0300

    Initial Commit

---
+++ LayoutAPITest/LayoutAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAPITestApp.swift
+//  LayoutAPITest
+//
+//  Created by Daniel Spalek on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
